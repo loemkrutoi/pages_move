c3506a6b47773ff68cda2c5a1b98cd08389aae9a
= <=>
2025-03-10 15:14:13 +0200

    test

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Project</title>
+</head>
+<body>
+
+    <div class="my-header container">
+            <nav class="navbar navbar-expand-lg position-fixed">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link text-light" href="#">SECTION 1</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link text-light" href="#">SECTION 2</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link text-light" href="#">SECTION 3</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    <div class="my-main m-auto container w-100 vh-100 row flex justify-content-around align-items-center">
+        <div class="left col-5 p-0">
+            <div class="top text-center">
+                <p>кого перетащить</p>
+            </div>
+            <div class="bottom row p-4 w-100 mx-auto">
+                <div class="my-page-block col-12 mt-2">
+                    <p class="text-center">какая нибудь страничка</p>
+                </div>
+                <div class="my-page-block col-12 mt-2">
+                    <p class="text-center">какая нибудь страничка</p>
+                </div>
+                <div class="my-page-block col-12 mt-2">
+                    <p class="text-center">какая нибудь страничка</p>
+                </div>
+            </div>
+        </div>
+        <div class="right col-5 p-0">
+            <div class="top text-center">
+                <p>куда перетащить</p>
+            </div>
+            <div class="bottom row p-4 w-100 mx-auto"></div>
+        </div>
+    </div>
+    <div class="my-footer container"></div>
+
+    <?php 
+    
+
+    
+    ?>
+</body>
+</html>
